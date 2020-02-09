ad8c6374899615436ec95b4fab6abd6215acccba
박현우 <gusn0905@gmail.com>
2020-02-10 08:33:51 +0900

    2020.02.09

--- Counting_sort.py
+++ Counting_sort.py
@@ -21,3 +21,13 @@ def Counting_Sort(unsorted_list, sorted_list, k):
 a = [0,4,1,3,1,2,4,1]
 b = [0] * len(a)
 print(Counting_Sort(a,b,max(a)+1))
+# cnt_list = [0,0,0,0,0]
+# cnt_list = [1,4,5,6,8]
+# sorted_list[cnt_list[1]-1] = sorted_list[3] = 1, cnt_list = [1,3,5,6,8] , sorted_list = [0,0,0,1,0,0,0,0]
+# sorted_list[cnt_list[4]-1] = sorted_list[7] = 4, cnt_list = [1,3,5,6,7] , sorted_list = [0,0,0,1,0,0,0,4]
+# sorted_list[cnt_list[2]-1] = sorted_list[4] = 2, cnt_list = [1,3,4,6,7] , sorted_list = [0,0,0,1,2,0,0,4]
+# sorted_list[cnt_list[1]-1] = sorted_list[2] = 1, cnt_list = [1,2,4,6,7] , sorted_list = [0,0,1,1,2,0,0,4]
+# sorted_list[cnt_list[3]-1] = sorted_list[5] = 3, cnt_list = [1,2,4,5,7] , sorted_list = [0,0,1,1,2,3,0,4]
+# sorted_list[cnt_list[1]-1] = sorted_list[1] = 1, cnt_list = [1,1,4,6,7] , sorted_list = [0,1,1,1,2,3,0,4]
+# sorted_list[cnt_list[4]-1] = sorted_list[6] = 4, cnt_list = [1,1,4,6,6] , sorted_list = [0,1,1,1,2,3,4,4]
+# sorted_list[cnt_list[0]-1] = sorted_list[0] = 0, cnt_list = [0,1,4,6,7] , sorted_list = [0,1,1,1,2,3,4,4]
